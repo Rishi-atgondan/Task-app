23caf8d8e3ba52e2eab46a183879dfc842695a6f
Rishi-atgondan <rishijeeva13@gmail.com>
2021-11-30 17:06:10 +0530

    added new files

---
+++ javascript/taskapp.js
@@ -0,0 +1,16 @@
+let mainList = [];
+
+// assigning values to main list
+function clicked() {
+    event.preventDefault();
+    let input = document.getElementById('mainInput');
+    mainList.unshift(input.value);
+    console.log(mainList);
+    let output = '';
+    for (i = 0; i < mainList.length; i++) {
+        let item = mainList[i];
+        let newOutput = '<li>' + item + '</li>';
+        output = output + newOutput;
+    }
+    document.getElementById('ul').innerHTML = output;
+}
